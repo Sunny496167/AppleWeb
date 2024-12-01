22cfe0a2926957cd79d2495968b1f27385487ba6
Sunny Kumar <sunnykumar49617@gmail.com>
2024-12-02 00:55:53 +0530

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import Hero from "./Components/Hero";
+import Highlights from "./Components/Highlights";
+import Navbar from "./Components/Navbar";
+
 export default function App() {
   return (
     <main>
-      Navbar
-      Hero
-      Highlights
+      <Navbar />
+      <Hero />
+      <Highlights />
     </main>
   )
 }
